7c28c717d296187b1886447cdd6864de41fa891b
Joshua952757 <jacobsjoshua1610@gmail.com>
2025-08-04 09:48:24 +0200

    Added docstrings to my forms.py

--- bronewsapp/forms.py
+++ bronewsapp/forms.py
@@ -4,18 +4,21 @@ from django.contrib.auth.models import User
 
 
 class PublisherForm(forms.ModelForm):
+    """Form for creating and updating Publisher objects."""
     class Meta:
         model = Publisher
         fields = ['name', 'content']
 
 
 class ArticleForm(forms.ModelForm):
+    """Form for creating and updating Article objects."""
     class Meta:
         model = Article
         fields = ['title', 'content', 'publisher']
-        
-    
+
+
 class NewsletterForm(forms.ModelForm):
+    """Form for creating and updating Newsletter objects."""
     class Meta:
         model = Newsletter
         fields = ['title', 'content', 'publisher']
